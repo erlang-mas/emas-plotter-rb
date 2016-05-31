9fbae521e5942b9afd0e1f9b84dda43188251f0e
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-06-01 01:57:14 +0200

    Log processing steps

--- lib/emas/plotter/runner.rb
+++ lib/emas/plotter/runner.rb
@@ -8,13 +8,17 @@ module EMAS
       end
 
       def run
+        $stdout.puts 'Building database'
         database = DB::Builder.new.build_database
 
+        $stdout.puts 'Loading results'
         results_loader = ResultsLoader.new database, results_dir
         results_loader.load_results
 
+        $stdout.puts 'Performing results aggregation'
         data_points = Aggregator.new(database).aggregate
 
+        $stdout.puts 'Plotting'
         plot = Plot.new data_points
         plot.draw
       end
