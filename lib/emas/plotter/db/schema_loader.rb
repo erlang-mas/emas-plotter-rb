ea9a21371a6ff495ab2731007dd8d4d93934d2ee
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-05-31 23:21:24 +0200

    Add aggregation tables to schema loader

--- lib/emas/plotter/db/schema_loader.rb
+++ lib/emas/plotter/db/schema_loader.rb
@@ -17,12 +17,15 @@ module EMAS
         def create_database_tables
           create_experiments_table
           create_results_table
+          create_reproductions_per_node_table
+          create_reproductions_per_second_table
         end
 
         def create_experiments_table
           database.create_table :experiments do
             primary_key :id
-            column      :name, :string
+            column      :name,        :string
+            column      :nodes_count, :integer
           end
         end
 
@@ -33,6 +36,26 @@ module EMAS
             column      :node,          :string
             column      :island,        :string
             column      :metric,        :string
+            column      :second,        :integer
+            column      :value,         :float
+          end
+        end
+
+        def create_reproductions_per_node_table
+          database.create_table :reproductions_per_node do
+            primary_key :id
+            foreign_key :experiment_id, :experiments
+            column      :node,          :string
+            column      :second,        :integer
+            column      :value,         :float
+          end
+        end
+
+        def create_reproductions_per_second_table
+          database.create_table :reproductions_per_second do
+            primary_key :id
+            foreign_key :experiment_id, :experiments
+            column      :node,          :string
             column      :second,        :integer
             column      :value,         :float
           end
