f0a1c6c86acc56287711ce44262208b20cff544e
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-05-31 20:24:49 +0200

    Add DB::SchemaLoader

--- lib/emas/plotter.rb
+++ lib/emas/plotter.rb
@@ -1,5 +1,7 @@
 require 'emas/plotter/version'
 
+require 'emas/plotter/db/schema_loader'
+
 module EMAS
   module Plotter
   end
---
+++ lib/emas/plotter/db/schema_loader.rb
@@ -0,0 +1,43 @@
+module EMAS
+  module Plotter
+    module DB
+      class SchemaLoader
+        attr_reader :database
+
+        def initialize(database)
+          @database = database
+        end
+
+        def load_schema
+          create_database_tables
+        end
+
+        private
+
+        def create_database_tables
+          create_experiments_table
+          create_results_table
+        end
+
+        def create_experiments_table
+          database.create_table :experiments do
+            primary_key :id
+            column      :name, :string
+          end
+        end
+
+        def create_results_table
+          database.create_table :results do
+            primary_key :id
+            foreign_key :experiment_id, :experiments
+            column      :node,          :string
+            column      :island,        :string
+            column      :metric,        :string
+            column      :second,        :integer
+            column      :value,         :float
+          end
+        end
+      end
+    end
+  end
+end
